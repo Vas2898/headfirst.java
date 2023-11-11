5a0e1da5d33d888e4f5d172dcebd7c71ce0d7875
vasanthakumar <109866822+Vas2898@users.noreply.github.com>
2023-11-11 17:34:49 +0530

    Write a Java program to print 'Hello' on screen and your name on a separate line

---
+++ exercise.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class W3{
+public static void main(String[] args){
+Scanner input = new Scanner(System.in);
+System.out.println("First Name : ");
+String firstname = input.next();
+System.out.println("Last Name: ");
+String lastname = input.next();
+
+System.out.println("Hello\n"+firstname+lastname);
+
+}
+}
